3ea8ae1d949c57adfbe36d3c107974453a7ff529
mohamed.ayman1 <mohamedayman@hotmail.com>
2023-02-02 23:28:35 +0200

    Adding a button and it's working now

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,9 +7,11 @@ def index():
     menu_options = ["Option 1", "Option 2", "Option 3"]
     return render_template("index.html", menu_options=menu_options)
 
+@app.route("/handle_button", methods=["POST"])
 def handle_button():
-    # Perform some action here
-    return "Button pressed!"
-
+    selected_option = request.form["dropdown"]
+    # Perform some action based on the selected option
+#    return "Button pressed! Selected option: {}".format(selected_option)
+    return f"You selected: {selected_option}"
 if __name__ == "__main__":
     app.run()
