baafab7aa191df1601e928e9adcd7c49fb748ea9
mohamed.ayman1 <mohamedayman@hotmail.com>
2023-02-02 21:13:18 +0200

    Adding a button

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ def index():
     menu_options = ["Option 1", "Option 2", "Option 3"]
     return render_template("index.html", menu_options=menu_options)
 
+def handle_button():
+    # Perform some action here
+    return "Button pressed!"
+
 if __name__ == "__main__":
     app.run()
